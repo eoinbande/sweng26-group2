8d441f5d525f1cf344c4f4e9a15e131ba119e62d
Liliana Berenguer <berengul@tcd.ie>
2026-01-30 22:55:03 +0000

    added login and signup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 import './index.css';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
