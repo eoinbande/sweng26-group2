b188ff8d601639290036b46f017f41269241828a
gian-dln <giandln7740@gmail.com>
2026-02-01 21:47:24 +0000

    add page template

---
+++ frontend/src/pages/ReviewPlan.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { ArrowRight } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+import BottomNav from '../components/BottomNav';
+import '../index.css';
+
+function ReviewPlan () {
+
+
+
+
+}
+
+export default ReviewPlan;
