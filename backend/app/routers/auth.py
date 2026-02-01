2b9f2caf5b1c9a5c8e2dee45b1fb74e507e2cb6f
Hakan Okray <okraysih@tcd.ie>
2026-02-01 18:19:24 +0000

    fix some issues, Auth done and ready for revision!

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -2,3 +2,29 @@ from fastapi import APIRouter
 from ..Tables import create_user #we import the functions related to Auth
 from pydantic import BaseModel
 
+#CREATION of USER(when a person created a new account) endpoint
+#USE GET(FrontEnd will send a request to GET data from Supabase)
+#USE POsT(FrontEnd will send a request to POST data to Supabase)
+
+account_router = APIRouter()
+
+class RequestUser(BaseModel):
+    user_id: str
+    name: str
+    email: str
+    
+
+#USE POST HTTP REQUEST TO STORE DATA IN SUPABASE
+@account_router.post("/profiles")
+def write_account(user: RequestUser):
+    result = create_user(
+        user_id = user.user_id,
+        name = user.name,
+        email = user.email
+    )
+    return {"message": "User successfully created", "user": result.data}
+
+
+#since user is creating account, we dont need to GET data, only validate it
+
+
