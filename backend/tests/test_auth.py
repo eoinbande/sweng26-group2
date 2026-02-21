c185e7a91284d405968f35709ace9ce0ca0348de
Hakan Okray <okraysih@tcd.ie>
2026-02-21 13:26:47 +0000

    created a test function to check if there are no errors while creating a valid user

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -6,7 +6,24 @@ client = TestClient(app)
 
 #TEST AUTHENTICATION: (AT LEAST 5 TESTS) try to get full coverage in profile creation
 
+#We will run the test by mocking the DB
 
 #Test: check if we can successfully create an user
-def test_user_creation():
+def test_user_creation(mock_create_user):
+    mock_create_user.return_value.data = [
+        {
+            "user_id": "123",
+            "name": "Alex",
+            "email": "alex@test.com"
+        }
+    ]
 
+    response = client.post("/profiles", json = {
+        "user_id": "123",
+        "name": "Alex",
+        "email": "alex@test.com"
+    })
+
+    assert response.status_code == 200 #check if we get the correct code
+    assert response.json()["message"] == "User successfully created"
+    assert response.json()["user"][0]["email"] == "alex@test.com"
