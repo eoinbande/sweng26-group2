e688d4741bbc7c1088378d68a9434210a16e650d
Hakan Okray <okraysih@tcd.ie>
2026-03-15 00:43:26 +0000

    added a new function in the green router to get green metrics per month

--- backend/app/routers/green.py
+++ backend/app/routers/green.py
@@ -42,4 +42,38 @@ def carbon_per_goal(user_id: str):
         goals[goal_id]["tokens"] += log["tokens_used"]
         goals[goal_id]["carbon"] += log["carbon_footprint"]
 
-        return goals
+    return goals
+    
+
+
+@green_router.get("/green/stats/monthlt/{user_id}")
+def get_monthly_green_stats(user_id: str):
+
+    response = supabase.table("ai_usage_logs")\
+    .select("*")\
+    .eq("user_id", user_id)\
+    .execute()
+
+    logs = response.data or []
+    monthly_stats = {}
+
+    for log in logs:
+        month = log["timestamp"][:7] #take the first 7 characters, supabase format YYYY-MM
+        if month not in monthly_stats:
+            monthly_stats[month] = {
+                "ai_calls": 0,
+                "tokens_used": 0,
+                "carbon_footprint": 0
+            }
+
+        monthly_stats[month]["ai_calls"] += 1
+        monthly_stats[month]["tokens_used"] += log["tokens_used"]
+        monthly_stats[month]["carbon_footprint"] += log["carbon_footprint"]
+
+    #when getting the sum, round carbon values
+    for month in monthly_stats:
+        monthly_stats[month]["carbon_footprint"] = round(
+            monthly_stats[month]["carbon_footprint"], 6
+        )
+
+    return monthly_stats
