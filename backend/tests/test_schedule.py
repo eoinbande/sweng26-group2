a30fe88ece32d5f06e1db0cfd240a65693c3b52d
ryodowd <ryodowd@tcd.ie>
2026-03-25 16:32:31 +0000

    began creating mock data structure for tests

--- backend/tests/test_schedule.py
+++ backend/tests/test_schedule.py
@@ -9,3 +9,12 @@ covering tasks by date, upcoming tasks, and upcoming goals.
 MOCKING DATABASE for all tests."""
  
 client = TestClient(app)
+
+#sample mock data
+
+TODAY = datetime.utcnow().strftime("%Y-%m-%d")
+YESTERDAY = (datetime.utcnow() - timedelta(days=1)).strftime("%Y-%m-%d")
+IN_7_DAYS = (datetime.utcnow() + timedelta(days=7)).strftime("%Y-%m-%d")
+IN_14_DAYS = (datetime.utcnow() + timedelta(days=14)).strftime("%Y-%m-%d")
+IN_20_DAYS = (datetime.utcnow() + timedelta(days=20)).strftime("%Y-%m-%d")
+IN_35_DAYS = (datetime.utcnow() + timedelta(days=35)).strftime("%Y-%m-%d")
